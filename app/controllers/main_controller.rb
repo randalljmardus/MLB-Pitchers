0daf8073f522976a7bbffc9be4e37d88605ce2a9
Randall Mardus <randallmardus@Randall-Marduss-MacBook.local>
2014-05-06 17:50:59 -0400

    tuesday homework commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,11 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def contact
+  	
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Knuckleball::Application.routes.draw do
+  get "main/index"
+  get "main/contact"
+  get "main/about"
   resources :players
+
+  root 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
